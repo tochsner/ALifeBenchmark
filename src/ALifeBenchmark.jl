3aac3af56f1427509f726f35e790b29741979a13
Tobia Ochsner <tobia.ochnser@hotmail.com>
2021-06-01 09:34:11 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.16

---
+++ src/ALifeBenchmark.jl
@@ -0,0 +1,5 @@
+module ALifeBenchmark
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using ALifeBenchmark
+using Test
+
+@testset "ALifeBenchmark.jl" begin
+    # Write your tests here.
+end
