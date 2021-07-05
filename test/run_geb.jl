18b70ce342992a7add968f7b137113b9ee21868b
Tobia Ochsner <ochsneto@eu-login-10.euler.ethz.ch>
2021-07-05 17:34:54 +0200

    Changed config on Euler

--- test/run_geb.jl
+++ test/run_geb.jl
@@ -7,12 +7,12 @@ using Profile
 
 create_model(logger) = GebModel(logger)
 function print_function(trial, step, model)
-    if step % 1_000 == 0
+    if step % 10_000 == 0
         @printf "%i %i (%s): Num of organisms: %i \n" trial step Dates.format(now(), "HH:MM:SS") length(model.organisms)
     end
 end
 
-const NUM_ITERATIONS = 1e7
+const NUM_ITERATIONS = 1e6
 const NUM_TRIALS = 1
 
 collect_distribution(create_model, execute!, print_function, NUM_ITERATIONS, NUM_TRIALS, 1)
